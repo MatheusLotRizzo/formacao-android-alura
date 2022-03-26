d756f53b0fbdfa3027ccf3da44dbc9610eac5d84
Matheus Rizzo <matheus.l.rizzo@gmail.com>
2022-03-26 02:37:21 -0300

    Inicio Projeto

---
+++ Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,33 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Toast.makeText(this,"Matheus Rizzo Studio",Toast.LENGTH_SHORT).show();
+
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de Alunos");
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Matheus","Jose","Vitor","Bia","Antonio"));
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
+    }
+}
